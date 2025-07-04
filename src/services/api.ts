515fd00430035a56fa98033220b0e458ab700ab8
salmannizam <salmannizam.info@gmail.com>
2025-07-04 15:05:30 +0530

    api url chnge

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 // src/services/api.ts
 import axios from 'axios';
 
-const API_BASE_URL = 'https://surveyciplapi-gefsbkc0dpgkgzdr.centralus-01.azurewebsites.net'; // Your NestJS server URL
+const API_BASE_URL = 'http://77.37.45.105:3005/'; // Your NestJS server URL
 
 // Create axios instance
 const api = axios.create({
